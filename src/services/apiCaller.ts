cf9c9e5677e6e2919b901b28bface447c63fb237
Benjamin Bascary <benjaminbascary@gmail.com>
2022-06-25 18:01:38 -0300

    This commit starts building the API handler that consists in a main class with private attributes. This will handle all data coming from the main endpoint.

---
+++ src/services/apiCaller.ts
@@ -0,0 +1,15 @@
+class ApiHandler {
+
+    private defaultEndpoint: string[] = ['https://rickandmortyapi.com/api'];
+    private endpoints: string[] = [];
+    private defaultPage: number = 1;
+
+    constructor() {}
+
+    getCharacters() {
+
+    }
+        
+}
+
+export const rickAndMortyAPIcaller = new ApiHandler();
